a6a7b6032596a5e0f162f93d99f88ee189e09181
MashaOsipova <mashaosi395@mail.ru>
2021-02-14 14:55:41 +0300

    first commit

---
+++ Untitled/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int transferSum = 500000000;
+        int totalVa = balance + transferSum;
+        System.out.println(totalVa);
+    }
+}
